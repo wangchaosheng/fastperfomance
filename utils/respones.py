e84e22d30a165d2d561c686c3fbe82b4369f5d99
qingwu <1134393789@qq.com>
2021-08-30 11:17:11 +0800

    删除多余代码

--- utils/respones.py
+++ utils/respones.py
@@ -4,6 +4,7 @@
 import json
 import requests
 
+json.dumps()
 SUCCESS = {
     'msg': 'success',
     'status': 0
@@ -18,15 +19,12 @@ def debug_request(obj):
         if 'params' in str(data):
             res = requests.request(method=data['method'].lower(), url=data['url'], headers=data['headers'],
                                    params=data['params']).json()
-            return res
         elif 'application/json' in str(data['headers']):
             res = requests.request(method=data['method'].lower(), url=data['url'], headers=data['headers'],
                                    json=data['json']).json()
-            return res
-
         else:
             res = requests.request(method=data['method'].lower(), url=data['url'], headers=data['headers'],
                                    data=data['data']).json()
-            return res
+        return res
     except Exception:
         return {'msg': '请求信息填写错误，请核对后再次尝试', 'status': 1}
